3343d50764d17931fa5e92d1a821524925730d5a
unknown <antonyvidetta1986@gmail.com>
2023-10-04 17:16:30 +1300

    Add details page

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,53 @@
 import { Link, Outlet } from 'react-router-dom';
-
+import { useQuery } from '@tanstack/react-query';
 import Header from '../Header.jsx';
+import { fetchEvent, deleteEvent } from '../../util/http';
+import { useParams } from 'react-router-dom';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
+
+  const params = useParams();
+  const id = params.id;
+  // const [id, setId] = useState();
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events-details', {id}],
+    queryFn: ({signal}) => fetchEvent({id, signal}),
+  });
+
   return (
     <>
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
-          View all Events
+          {data.title}
         </Link>
       </Header>
-      <article id="event-details">
+      {isPending && <p>Loading ...</p>}
+      {isError && <ErrorBlock title="error on details" message="try again later"/>}
+      {data && 
+        <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} @ {data.time}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data.description}</p>
           </div>
         </div>
       </article>
+      }
+
     </>
   );
 }
