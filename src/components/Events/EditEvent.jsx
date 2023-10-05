eac11563d887e83a2bdd1ab758bb494c458e1653
unknown <antonyvidetta1986@gmail.com>
2023-10-05 20:21:10 +1300

    Add all

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useQuery, useMutation } from '@tanstack/react-query';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { useQuery, useMutation, useQueryClient, QueryClient } from '@tanstack/react-query';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
@@ -11,13 +11,18 @@ export default function EditEvent() {
   const params = useParams();
   const id = params.id;
 
-  const { data, isPending, isError, error } = useQuery({
+  const  { data, isPending, isError, error } = useQuery({
     queryKey: ['events', id],
     queryFn: ({signal}) => fetchEvent({signal, id})
   });
 
   const { mutate } = useMutation({
-    mutationFn: updateEvent
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const eventData = data.event;
+      await queryClient.cancelQueries({queryKey: ['events', params.id]})
+      queryClient.setQueriesData(['events', params.id], eventData);
+    }
   });
 
   
